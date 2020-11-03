f1809de36bcc8a40264dc73ea4889e8281fca524
Felix Hao <felixhao28@gmail.com>
2020-11-03 15:47:04 +0800

    new node-systray

--- src/index.ts
+++ src/index.ts
@@ -1,199 +1,248 @@
-import * as child from 'child_process'
-import * as path from 'path'
-import * as os from 'os'
-import * as fs from 'fs-extra'
-import * as EventEmitter from 'events'
-import * as readline from 'readline'
-import Debug from 'debug'
+import * as child from "child_process";
+import * as path from "path";
+import * as os from "os";
+import * as fs from "fs-extra";
+import * as readline from "readline";
 
-const pkg = require('../package.json')
-const debug = Debug(pkg.name)
-
-export type MenuItem = {
-  title: string,
-  tooltip: string,
-  checked: boolean,
-  enabled: boolean,
+function debug(msgType: string, ...msg: any[]) {
+  // console.log(msgType + ":" + msg.map(m => {
+  //   let t = typeof (m) === "string" ? m : JSON.stringify(m);
+  //   const p = t.indexOf("\"icon\":");
+  //   if (p >= 0) {
+  //     const e = t.indexOf("\"", p + 8);
+  //     t = t.substring(0, p + 8) + "<ICON>" + t.substring(e);
+  //   }
+  //   const limit = 500;
+  //   if (t.length > limit) {
+  //     t = t.substring(0, limit / 2) + "..." + t.substring(t.length - limit / 2);
+  //   }
+  //   return t;
+  // }).join(" "));
+  return;
 }
 
-export type Menu = {
-  icon: string,
-  title: string,
-  tooltip: string,
-  items: MenuItem[],
+export interface MenuItem {
+  title: string;
+  tooltip: string;
+  checked?: boolean;
+  enabled: boolean;
+  hidden?: boolean;
 }
 
-export type ClickEvent = {
-  type: 'clicked',
-  item: MenuItem,
-  seq_id: number,
+export interface Menu {
+  icon: string;
+  title: string;
+  tooltip: string;
+  items: MenuItem[];
 }
 
-export type ReadyEvent = {
-  type: 'ready',
+export interface ClickEvent {
+  type: "clicked";
+  item: MenuItem;
+  seq_id: number;
 }
 
-export type Event = ClickEvent | ReadyEvent
-
-export type UpdateItemAction = {
-  type: 'update-item',
-  item: MenuItem,
-  seq_id: number,
+export interface ReadyEvent {
+  type: "ready";
 }
 
-export type UpdateMenuAction = {
-  type: 'update-menu',
-  menu: Menu,
-  seq_id: number,
+export type Event = ClickEvent | ReadyEvent;
+
+export interface UpdateItemAction {
+  type: "update-item";
+  item: MenuItem;
+  seq_id: number;
 }
 
-export type UpdateMenuAndItemAction = {
-  type: 'update-menu-and-item',
-  menu: Menu,
-  item: MenuItem,
-  seq_id: number,
+export interface UpdateMenuAction {
+  type: "update-menu";
+  menu: Menu;
+  seq_id: number;
 }
 
-export type Action = UpdateItemAction | UpdateMenuAction | UpdateMenuAndItemAction
-
-export type Conf = {
-  menu: Menu,
-  debug?: boolean,
-  copyDir?: boolean | string
+export interface UpdateMenuAndItemAction {
+  type: "update-menu-and-item";
+  menu: Menu;
+  item: MenuItem;
+  seq_id: number;
 }
 
-const getTrayBinPath = (debug: boolean = false, copyDir: boolean | string = false) => {
+export interface ExitAction {
+  type: "exit";
+}
+
+export type Action = UpdateItemAction | UpdateMenuAction | UpdateMenuAndItemAction | ExitAction;
+
+export interface Conf {
+  menu: Menu;
+  debug?: boolean;
+  copyDir?: boolean | string;
+}
+
+const getTrayBinPath = async (debug: boolean = false, copyDir: boolean | string = false) => {
   const binName = ({
-    win32: `tray_windows${debug ? '' : '_release'}.exe`,
-    darwin: `tray_darwin${debug ? '' : '_release'}`,
-    linux: `tray_linux${debug ? '' : '_release'}`,
-  })[process.platform]
-  const binPath = path.resolve(`${__dirname}/../traybin/${binName}`)
+    win32: `tray_windows${debug ? "" : "_release"}.exe`,
+    darwin: `tray_darwin${debug ? "" : "_release"}`,
+    linux: `tray_linux${debug ? "" : "_release"}`,
+  })[process.platform];
+  const binPath = path.resolve(`./traybin/${binName}`);
   if (copyDir) {
     copyDir = path.join((
-      typeof copyDir === 'string'
+      typeof copyDir === "string"
         ? copyDir
-        : `${os.homedir()}/.cache/node-systray/`), pkg.version)
+        : `${os.homedir()}/.cache/node-systray/`), "tmp");
 
-    const copyDistPath = path.join(copyDir, binName)
-    if (!fs.existsSync(copyDistPath)) {
-      fs.ensureDirSync(copyDir)
-      fs.copySync(binPath, copyDistPath)
+    const copyDistPath = path.join(copyDir, binName);
+    try {
+      await fs.stat(copyDistPath);
+    } catch (error) {
+      await fs.ensureDir(copyDistPath);
+      await fs.copy(binPath, copyDistPath);
     }
 
-    return copyDistPath
+    return copyDistPath;
   }
-  return binPath
-}
-const CHECK_STR = ' (√)'
+  return binPath;
+};
+const CHECK_STR = " (√)";
 function updateCheckedInLinux(item: MenuItem) {
-  if (process.platform !== 'linux') {
-    return item
+  if (process.platform !== "linux") {
+    return item;
   }
   if (item.checked) {
-    item.title += CHECK_STR
+    item.title += CHECK_STR;
   } else {
-    item.title = (item.title || '').replace(RegExp(CHECK_STR + '$'), '')
+    item.title = (item.title || "").replace(RegExp(CHECK_STR + "$"), "");
   }
-  return item
+  return item;
 }
 
-export default class SysTray extends EventEmitter {
-  protected _conf: Conf
-  protected _process: child.ChildProcess
-  protected _rl: readline.ReadLine
-  protected _binPath: string
+export default class SysTray {
+  protected _conf: Conf;
+  protected _process: child.ChildProcess;
+  protected _rl: readline.ReadLine;
+  protected _binPath: string;
 
   constructor(conf: Conf) {
-    super()
-    this._conf = conf
-    this._binPath = getTrayBinPath(conf.debug, conf.copyDir)
+    this._conf = conf;
+    this._process = null!;
+    this._rl = null!;
+    this._binPath = null!;
+  }
+
+  async init() {
+    const conf = this._conf;
+    this._binPath = await getTrayBinPath(conf.debug, conf.copyDir);
+    if (process.platform == "win32") {
+      try {
+        await fs.stat(this._binPath);
+      } catch (error) {
+        child.exec("cscript alert-win.vbs");
+      }
+    }
+    try {
+      await fs.chmod(this._binPath, "+x");
+    } catch (error) {
+      // ignore
+    }
     this._process = child.spawn(this._binPath, [], {
-      windowsHide: true
-    })
+      windowsHide: true,
+    });
     this._rl = readline.createInterface({
-      input: this._process.stdout,
-    })
-    conf.menu.items = conf.menu.items.map(updateCheckedInLinux)
-    this._rl.on('line', data => debug('onLine', data))
-    this.onReady(() => this.writeLine(JSON.stringify(conf.menu)))
+      input: this._process.stdout!,
+    });
+    conf.menu.items = conf.menu.items.map(updateCheckedInLinux);
+    this._rl.on("line", data => debug("onLine", data));
+    this.onReady(() => this.writeLine(JSON.stringify(conf.menu)));
   }
 
   onReady(listener: () => void) {
-    this._rl.on('line', (line: string) => {
-      let action: Event = JSON.parse(line)
-      if (action.type === 'ready') {
-        listener()
-        debug('onReady', action)
+    this._rl.on("line", (line: string) => {
+      const action: Event = JSON.parse(line);
+      if (action.type === "ready") {
+        listener();
+        debug("onReady", action);
       }
-    })
-    return this
+    });
+    return this;
   }
 
   onClick(listener: (action: ClickEvent) => void) {
-    this._rl.on('line', (line: string) => {
-      let action: ClickEvent = JSON.parse(line)
-      if (action.type === 'clicked') {
-        debug('onClick', action)
-        listener(action)
+    this._rl.on("line", (line: string) => {
+      const action: ClickEvent = JSON.parse(line);
+      if (action.type === "clicked") {
+        debug("onClick", action);
+        listener(action);
       }
-    })
-    return this
+    });
+    return this;
   }
 
   writeLine(line: string) {
     if (line) {
-      debug('writeLine', line + '\n', '=====')
-      this._process.stdin.write(line.trim() + '\n')
+      debug("writeLine", line + "\n", "=====");
+      this._process.stdin!.write(line.trim() + "\n");
     }
-    return this
+    return this;
   }
 
   sendAction(action: Action) {
     switch (action.type) {
-      case 'update-item':
-        action.item = updateCheckedInLinux(action.item)
-        break
-      case 'update-menu':
-        action.menu.items = action.menu.items.map(updateCheckedInLinux)
-        break
-      case 'update-menu-and-item':
-        action.menu.items = action.menu.items.map(updateCheckedInLinux)
-        action.item = updateCheckedInLinux(action.item)
-        break
+      case "update-item":
+        action.item = updateCheckedInLinux(action.item);
+        break;
+      case "update-menu":
+        action.menu.items = action.menu.items.map(updateCheckedInLinux);
+        break;
+      case "update-menu-and-item":
+        action.menu.items = action.menu.items.map(updateCheckedInLinux);
+        action.item = updateCheckedInLinux(action.item);
+        break;
     }
-    debug('sendAction', action)
-    this.writeLine(JSON.stringify(action))
-    return this
+    debug("sendAction", action);
+    this.writeLine(JSON.stringify(action));
+    return this;
   }
   /**
    * Kill the systray process
    * @param exitNode Exit current node process after systray process is killed, default is true
    */
-  kill(exitNode = true) {
-    if (exitNode) {
-      this.onExit(() => process.exit(0))
-    }
-    this._rl.close()
-    this._process.kill()
+  async kill(exitNode = true) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.onExit(() => {
+          resolve();
+          if (exitNode) {
+            process.exit(0);
+          }
+        });
+        this.sendAction({
+          type: "exit"
+        });
+        // this._rl.close();
+        // this._process.kill();
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   onExit(listener: (code: number | null, signal: string | null) => void) {
-    this._process.on('exit', listener)
+    this._process.on("exit", listener);
   }
 
   onError(listener: (err: Error) => void) {
-    this._process.on('error', err => {
-      debug('onError', err, 'binPath', this.binPath)
-      listener(err)
-    })
+    this._process.on("error", err => {
+      debug("onError", err, "binPath", this.binPath);
+      listener(err);
+    });
   }
 
   get killed() {
-    return this._process.killed
+    return this._process.killed;
   }
 
   get binPath() {
-    return this._binPath
+    return this._binPath;
   }
 }
